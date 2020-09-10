f04c4e561088eb79f56d7dfc1d16fd43134a10ca
Thanos Kastoras <thanoskastoras@gmail.com>
2020-09-11 01:31:36 +0300

    Add files via upload

---
+++ palindrome-checker.js
@@ -0,0 +1,27 @@
+function isAlphanumeric(character) {
+  if (character >= "a" && character <= "z") return true
+  if (character >= "A" && character <= "Z") return true
+  if (character >= "0" && character <= "9") return true
+  return false
+}
+
+function removeNonAlphanumeric(list) {
+  for (let i = 0; i < list.length; i++) {
+    if ( !isAlphanumeric(list[i]) ) {
+      list.splice(i-- , 1)
+    }
+  }
+  return list
+}
+
+function palindrome(str) {
+  let list = removeNonAlphanumeric(str.toLowerCase().split(""))
+
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] !== list[list.length - i - 1]) return false
+  }
+
+  return true
+}
+
+console.log(removeNonAlphanumeric("My age is 0, 0 si ega ym.".split("")))
