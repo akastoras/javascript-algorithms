348b5398c8ef976dd35a8c5f7b9d5c535906f909
Thanos Kastoras <thanoskastoras@gmail.com>
2020-10-01 01:23:38 +0300

    Create telephone-number-validator.js

---
+++ telephone-number-validator/telephone-number-validator.js
@@ -0,0 +1,16 @@
+var validFormats = [
+  /^(1*)\s{0,1}\d{3}-\d{3}-\d{4}$/,
+  /^(1*)\s{0,1}\(\d{3}\)\s{0,1}\d{3}-\d{4}$/,
+  /^(1*)\s{0,1}\d{10}$/,
+  /^(1*)\s{0,1}\d{3}\s\d{3}\s\d{4}$/
+]
+
+function telephoneCheck(str) {
+  return validFormats.reduce((isValid, format) => {
+    if (format.test(str)) {
+      return true;
+    } else {
+      return isValid
+    }
+  }, false);
+}
